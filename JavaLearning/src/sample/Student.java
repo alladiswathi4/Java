682ecade37556a7cb5b0a641a515d6c9ec8bd443
alladiswathi4 <113656966+alladiswathi4@users.noreply.github.com>
2022-09-18 22:51:54 -0500

    Create Student.java
    created student class

---
+++ JavaLearning/src/sample/Student.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Student {
+
+		String name;
+		
+		public void setName(String var) {
+			name = var;	
+		}
+		public String getName() {
+			return name;
+		}
+		
+
+}
