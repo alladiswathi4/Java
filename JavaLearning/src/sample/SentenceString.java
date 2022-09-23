cd341d4b551b4f1b15b0388b015c7417d43e29d1
alladiswathi4 <113656966+alladiswathi4@users.noreply.github.com>
2022-09-22 23:23:06 -0500

    Create SentenceString.java

---
+++ JavaLearning/src/sample/SentenceString.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class SentenceString {
+	public static void main(String[]args) {
+		String input="i am learning java";
+		String searchtext="java";
+		if(input.contains(searchtext)) {
+			System.out.println("text is their");
+			
+		}
+		else {
+			System.out.println("text is not their");
+			
+		}
+	}
+	
+
+}
